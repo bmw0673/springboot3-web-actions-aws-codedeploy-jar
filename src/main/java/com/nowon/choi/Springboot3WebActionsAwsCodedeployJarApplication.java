fb6aeb9837e26c85b6ddbe556198db3e754b3d0d
Administrator <Administrator@304-18>
2023-12-27 11:58:17 +0900

    1227

---
+++ src/main/java/com/nowon/choi/Springboot3WebActionsAwsCodedeployJarApplication.java
@@ -0,0 +1,13 @@
+package com.nowon.choi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3WebActionsAwsCodedeployJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3WebActionsAwsCodedeployJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nowon/choi/Springboot3WebActionsAwsCodedeployJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowon.choi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3WebActionsAwsCodedeployJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
